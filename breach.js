cd263fa21b500242c0661e1e76c9bb466f7a9eb0
s3id0n <jjohnanselmo@gmail.com>
2018-01-16 19:18:59 -0500

    Cracks all T1 locks

--- breach.js
+++ breach.js
@@ -1,14 +1,36 @@
 function(context, args) {
     var target = args.t;
     var r = target.call({});
+    var p = ["unlock", "open", "release"];
     var unlocked;
     var x = 0;
     var y = 0;
-    var c = 0;
-    var d = 0;
+
+    if(r.indexOf("EZ_21") > -1) {
+        x = 0;
+        while(r.indexOf("UNLOCKED") < 0) {
+            r = target.call({EZ_21: p[x]});
+            x++;
+        }
+    }
+
+    if(r.indexOf("EZ_35") > -1) {
+        x = 0;
+        while(r.indexOf("digit") < 0) {
+            r = target.call({EZ_35:p[x]});
+            unlocked = p[x];
+            x++;
+        }
+        while(r.indexOf("UNLOCKED") < 0) {
+            r = target.call({EZ_35:unlocked, digit:y})
+            y++;
+        }
+    }
+
     if (r.indexOf("EZ_40") > -1) {
+        x = 0;
+        y = 0;
         while (r.indexOf("ez_prime") < 0) {
-            var p = ["unlock", "open", "release"];
             r = target.call({ EZ_40: p[x] });
             unlocked = p[x]
             x++
@@ -24,20 +46,24 @@ function(context, args) {
     }
 
     if (r.indexOf("c002") > -1) {
+        x = 0;
         while (r.indexOf("UNLOCKED") < 0) {
             var c1 = ["red", "orange", "yellow", "lime", "green", "cyan", "blue", "purple"];
-            var c002 = { c002: c1[c], c002_complement: c1[(c + 4) % 8] };
+            var c002 = { c002: c1[x], c002_complement: c1[(x + 4) % 8] };
             Object.assign(c002, args);
             r = target.call(c002);
-            c++;
+            x++;
         }
     }
 
     if (r.indexOf("c003") > -1) {
+        x = 0;
         while (r.indexOf("UNLOCKED") < 0) {
             var c3 = ["red", "orange", "yellow", "lime", "green", "cyan", "blue", "purple"];
-            r = target.call({ c003: c3[d], c003_triad_1: c3[(d + 5) % 8], c003_triad_2: c3[(d + 3) % 8] });
-            d++
+            var c003 = { c003: c3[x], c003_triad_1: c3[(x + 5) % 8], c003_triad_2: c3[(x + 3) % 8] };
+            Object.assign(c003, args);
+            r = target.call(c003);
+            x++
         }
     }
     return r;
